598305e67b0a02c804247ff12689929189b09886
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2015-01-17 02:45:43 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
@@ -11,9 +15,9 @@ class StaticPagesController < ApplicationController
   def contact
   end
   
-#  def signup
- # end
+  def signup
+  end
 
-  #def login
-  #end
+  def login
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
-    def setup
+  def setup
     @user = users(:michael)
-    end
+  end
   
   test "login with invalid information" do
     get login_path
